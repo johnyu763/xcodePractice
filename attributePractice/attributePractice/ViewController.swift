5f3e8739db2b2adf6c33e7a738a6ab6643bc78b4
johnyu763 <johny11@uci.edu>
2019-08-29 21:05:21 -0700

    color slider finished

--- attributePractice/attributePractice/ViewController.swift
+++ attributePractice/attributePractice/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainGradient: GradientView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func handleColorChange(_ sender: UISlider) {
+        let red_num = UIColor(red: CGFloat(sender.value), green: 0, blue: 0, alpha: 1)
+        let other_color = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+        mainGradient.setColor(color1: red_num, color2: other_color)
+    }
 }
+
 

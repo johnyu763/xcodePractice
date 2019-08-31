31fe515a4a9ca0f8c05a8ee3362720837d48589c
johnyu763 <johny11@uci.edu>
2019-08-30 19:35:34 -0700

    continued attribute practice

---
+++ attributePractice2/attributePractice2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  attributePractice2
+//
+//  Created by John Yu on 8/30/19.
+//  Copyright © 2019 John Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
